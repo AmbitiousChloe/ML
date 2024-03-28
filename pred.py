4e8f587a3fbb1db0df6ee44aa292b6d3349652d2
kaifeng Li <likaifeng0516@gmail.com>
2024-03-28 15:30:06 -0400

    pred add

--- pred.py
+++ pred.py
@@ -20,7 +20,7 @@ def radm_dict(d):
             new_dict[key_to_keep] = value
         else:
             new_dict[keys[0]] = value
-    
+
     return new_dict
 
 
@@ -81,11 +81,11 @@ def process_data(filename: str) -> pd.DataFrame:
     # Q6 num
 #    df["Q6"] = df["Q6"].apply(lambda x: to_dict_complete(x))
    df["Q6_Skyscr"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Skyscrapers", 0))
-   df["Q6_Sport"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Sport", 0)))
-   df["Q6_AM"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Art and Music", 0)))
-   df["Q6_Carnival"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Carnival", 0)))
-   df["Q6_Cuisine"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Cuisine", 0)))
-   df["Q6_Eco"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Economic", 0)))
+   df["Q6_Sport"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Sport", 0))
+   df["Q6_AM"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Art and Music", 0))
+   df["Q6_Carnival"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Carnival", 0))
+   df["Q6_Cuisine"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Cuisine", 0))
+   df["Q6_Eco"] = df["Q6"].apply(lambda x: to_dict_complete(x).get("Economic", 0))
    
    df["Q7"] = df["Q7"].apply(challenge_basic.to_numeric)
    df["Q8"] = df["Q8"].apply(challenge_basic.to_numeric)
@@ -112,21 +112,26 @@ def process_data(filename: str) -> pd.DataFrame:
     # Q6_max_onehot = pd.get_dummies(df['Q6_max'], prefix='Q6_max', dtype=int)
     # df['Q6_min'] = pd.Categorical(df['Q6_min'], categories=Q6_categories)
     # Q6_min_onehot = pd.get_dummies(df['Q6_min'], prefix='Q6_min', dtype=int)
-    cities = ['Dubai', 'Rio de Janeiro', 'New York City', 'Paris']
-    city_to_number = {city: i for i, city in enumerate(cities)}
-    df['Label'] = df['Label'].map(city_to_number)
-    # df['Label'] = pd.Categorical(df['Label'], categories=cities)
-    # Label_onehot = pd.get_dummies(df['Label'], prefix='Label', dtype=int)
+   cities = ['Dubai', 'Rio de Janeiro', 'New York City', 'Paris']
+   city_to_number = {city: i for i, city in enumerate(cities)}
+   df['Label'] = df['Label'].map(city_to_number)
+   # df['Label'] = pd.Categorical(df['Label'], categories=cities)
+   # Label_onehot = pd.get_dummies(df['Label'], prefix='Label', dtype=int)
     
-    df = pd.concat([df, Q1_onehot, Q2_onehot, Q3_onehot, Q4_onehot, Q6_max_onehot, Q6_min_onehot], axis=1)
+   # df = pd.concat([df, Q1_onehot, Q2_onehot, Q3_onehot, Q4_onehot, Q6_max_onehot, Q6_min_onehot], axis=1)
+   df = pd.concat([df, Q1_onehot, Q2_onehot, Q3_onehot, Q4_onehot, Q6_Skyscr, Q6_Sport, Q6_AM, Q6_Carnival, Q6_Cuisine, Q6_Eco], axis=1)
 
-    df['Q7'] = (df['Q7'] - df['Q7'].mean()) / (df['Q7'].std() + 0.0001)
-    df['Q8'] = (df['Q8'] - df['Q8'].mean()) / (df['Q8'].std() + 0.0001)
-    df['Q9'] = (df['Q9'] - df['Q9'].mean()) / (df['Q9'].std() + 0.0001)
 
-    delete_columns = ['Q1', 'Q2', 'Q3', 'Q4', 'id', 'Q5', 'Q6', 'Q10', 'Q6_max', 'Q6_min'] # Edit Accordingly
-    for col in delete_columns:
-        del df[col]
+   df['Q7'] = (df['Q7'] - df['Q7'].mean()) / (df['Q7'].std() + 0.0001)
+   df['Q8'] = (df['Q8'] - df['Q8'].mean()) / (df['Q8'].std() + 0.0001)
+   df['Q9'] = (df['Q9'] - df['Q9'].mean()) / (df['Q9'].std() + 0.0001)
+
+   # delete_columns = ['Q1', 'Q2', 'Q3', 'Q4', 'id', 'Q5', 'Q6', 'Q10', 'Q6_max', 'Q6_min'] # Edit Accordingly
+   delete_columns = ['Q1', 'Q2', 'Q3', 'Q4', 'id', 'Q5', 'Q6', 'Q10'] # Edit Accordingly
+   for col in delete_columns:
+    del df[col]
+
+   return df
 
 
 def predict_all(file: str):
